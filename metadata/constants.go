606fd6ea12ed38f94910246d8627ebe8ad04a770
0xjackalope <huu@ninja.org>
2018-12-06 11:43:08 +0700

    tam

--- metadata/constants.go
+++ metadata/constants.go
@@ -4,4 +4,5 @@ const (
 	InvalidMeta = iota
 	LoanRequestMeta
 	BuySellRequestMeta
+	VoteDCBBoardMeta
 )
--- metadata/voteboard.go
+++ metadata/voteboard.go
@@ -1,10 +1,43 @@
 package metadata
 
-type VoteDCBBoard struct{
+import (
+	"github.com/ninjadotorg/constant/common"
+)
+
+type VoteDCBBoardMetadata struct {
 	CandidatePubKey string
+
+	Metadata
 }
-func (VoteDCBBoard) Validate() error {
+
+func NewVoteDCBBoardMetadata(voteDCBBoardMetadata map[string]interface{}) *VoteDCBBoardMetadata {
+	return &VoteDCBBoardMetadata{
+		CandidatePubKey: voteDCBBoardMetadata["candidatePubKey"].(string),
+	}
+}
+
+func (voteDCBBoardMetadata *VoteDCBBoardMetadata) Validate() error {
 	return nil
 }
 
-func ()
+func (voteDCBBoardMetadata *VoteDCBBoardMetadata) Process() error {
+	return nil
+}
+
+func (voteDCBBoardMetadata *VoteDCBBoardMetadata) CheckTransactionFee(tr TxRetriever, minFee uint64) bool {
+	return true
+}
+
+func (voteDCBBoardMetadata *VoteDCBBoardMetadata) ValidateTxWithBlockChain(bcr BlockchainRetriever, chainID byte) (bool, error) {
+	return true, nil
+}
+
+func (voteDCBBoardMetadata *VoteDCBBoardMetadata) GetType() int {
+	return VoteDCBBoardMeta
+}
+
+func (voteDCBBoardMetadata *VoteDCBBoardMetadata) Hash() *common.Hash {
+	record := voteDCBBoardMetadata.CandidatePubKey
+	hash := common.DoubleHashH([]byte(record))
+	return &hash
+}
